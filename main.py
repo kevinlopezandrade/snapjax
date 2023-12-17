6e968d38355f92502ccd8f5085e7e480fc244e89
Kev Lopez <kevinlopezandrade@gmail.com>
2023-12-17 15:44:38 +0100

    Test BPTT and RTRL with a standard RNN

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import timeit
 from functools import partial
+from typing import Any, Tuple
 
 import equinox as eqx
 import jax
@@ -7,55 +9,49 @@ from equinox.nn import State, StateIndex, make_with_state
 from jax import config
 from jaxtyping import Array
 
-from pta_cell import (
-    Any,
-    List,
-    PTACell,
-    PTALayerRTRL,
-    StackedRTRL,
-    zero_influence_pytree,
-)
+from rnn import RNN, StackedRNN
 
 config.update("jax_numpy_rank_promotion", "raise")
 
 
-def is_pta_cell_or_jacobian_index(node: Any):
-    if isinstance(node, PTACell) or isinstance(node, StateIndex):
+def is_rnn_cell_or_jacobian_index(node: Any):
+    if isinstance(node, RNN) or isinstance(node, StateIndex):
         return True
     else:
         return False
 
 
-def make_zeros_jacobian(model: StackedRTRL):
+def make_zeros_jacobian(model: StackedRNN):
     def zeros_in_leaf(leaf):
         if eqx.is_array(leaf):
             return jnp.zeros(shape=(model.hidden_size, *leaf.shape))
         else:
             return None
 
-    zero_influences = jax.tree_map(zeros_in_leaf, model_rtrl)
+    zero_influences = jax.tree_map(zeros_in_leaf, model)
 
     return zero_influences
 
 
-def step_loss_test(
-    model: StackedRTRL,
-    h_prev: Array,
-    x_t: Array,
-    perturbations: Array,
-    jacobians_state: eqx.nn.State,
-    y_t: Array,
-):
-    h_t, y_hat, new_state = model(h_prev, x_t, perturbations, jacobians_state)
+def make_zeros_grads(model: StackedRNN):
+    def zeros_in_leaf(leaf):
+        if eqx.is_array(leaf):
+            return jnp.zeros(shape=leaf.shape)
+        elif isinstance(leaf, StateIndex):
+            return None
+        else:
+            return None
 
-    diff = (y_t - y_hat) ** 2
+    zero_grads = jax.tree_map(
+        zeros_in_leaf, model, is_leaf=lambda leaf: isinstance(leaf, StateIndex)
+    )
 
-    return jnp.sum(diff), (h_t, new_state)
+    return zero_grads
 
 
 def step_loss(
-    model_spatial: StackedRTRL,
-    model_rtrl: StackedRTRL,
+    model_spatial: StackedRNN,
+    model_rtrl: StackedRNN,
     h_prev: Array,
     x_t: Array,
     perturbations: Array,
@@ -70,15 +66,37 @@ def step_loss(
     return jnp.sum(diff), (h_t, new_state)
 
 
+def sparse_multiplication(D_t: Array, J_t_prev: Array):
+    """
+    J_t_prev: Is of shape, [n_hidden_units, n_hidden_units, n_hidden_units]
+    """
+
+    def product(J_t_theta: Array, i: int):
+        res = jax.vmap(lambda J_t_theta_i_column: D_t[:, i] * J_t_theta_i_column[i])(
+            J_t_theta
+        )
+
+        return res
+
+    final = []
+    for i in range(J_t_prev.shape[0]):
+        final.append(product(J_t_prev[i], i))
+
+    final = jnp.stack(final)
+
+    return final
+
+
 # This should be a pure function.
 @eqx.filter_jit
 def forward_rtrl(
-    theta_spatial: StackedRTRL,
-    theta_rtrl: StackedRTRL,
+    theta_spatial: StackedRNN,
+    theta_rtrl: StackedRNN,
     h_prev: Array,
     input: Array,
     perturbations: Array,
-    jacobians_prev: StackedRTRL,
+    acc_grads: StackedRNN,
+    jacobians_prev: StackedRNN,
     inmediate_jacobians_state: eqx.nn.State,
     target: Array,
 ):
@@ -113,9 +131,7 @@ def forward_rtrl(
 
         jacobians = eqx.tree_at(lambda model: model.layers[i].cell, jacobians, J_t)
 
-        # SNAP 1, just keep the non zero entries that appear
-        # in the i_t.
-
+    grads = spatial_grads
     for i in range(model.num_layers):
         ht_grad = hidden_states_grads[i]
         rtrl_grads = jax.tree_map(
@@ -123,37 +139,117 @@ def forward_rtrl(
             jacobians.layers[i].cell,
         )
 
-        spatial_grads = eqx.tree_at(
+        grads = eqx.tree_at(
             lambda model: model.layers[i].cell,
-            spatial_grads,
+            grads,
             rtrl_grads,
             is_leaf=lambda x: x is None,
         )
 
-    return spatial_grads
+    acc_grads = jax.tree_map(lambda acc_grad, grad: acc_grad + grad, acc_grads, grads)
+
+    return h_t, acc_grads, jacobians, inmediate_jacobians_state, loss
 
 
-num_layers = 10
-hidden_size = 20
-input_size = 20
+def rtrl(
+    model: StackedRNN, inputs: Array, jacobian_state: eqx.nn.State, targets: Array
+):
+    model_rtrl, model_spatial = eqx.partition(
+        model,
+        lambda leaf: is_rnn_cell_or_jacobian_index(leaf),
+        is_leaf=is_rnn_cell_or_jacobian_index,
+    )
+    perturbations = jnp.zeros(shape=(model.num_layers, model.hidden_size))
+
+    def f_repack(carry, data):
+        h, acc_grads, jacobians, inmediate_jacobians_state = carry
+        input, target = data
+
+        h, acc_grads, jacobians, inmediate_jacobians_state, loss = forward_rtrl(
+            model_spatial,
+            model_rtrl,
+            h,
+            input,
+            perturbations,
+            acc_grads,
+            jacobians,
+            inmediate_jacobians_state,
+            target,
+        )
+
+        return (h, acc_grads, jacobians, inmediate_jacobians_state), loss
+
+    h_init = jnp.zeros(shape=(model.num_layers, model.hidden_size))
+    acc_grads = make_zeros_grads(model)
+    zero_jacobians = make_zeros_jacobian(model_rtrl)
+
+    carry_out, losses = jax.lax.scan(
+        f_repack,
+        init=(h_init, acc_grads, zero_jacobians, jacobian_state),
+        xs=(inputs, targets),
+    )
+
+    return jnp.sum(losses), carry_out[1]
+
+
+def forward_sequence(model: StackedRNN, inputs: Array, jacobian_state: eqx.nn.State):
+    hidden_state = jnp.zeros(shape=(model.num_layers, model.hidden_size))
+    perturbations = jnp.zeros(shape=(model.num_layers, model.hidden_size))
+
+    def f_repack(carry: Tuple[Array, eqx.nn.State], input: Array):
+        h, jacobian_state = carry
+        h, out, jacobian_state = model(h, input, perturbations, jacobian_state)
+        return (h, jacobian_state), out
+
+    # Ful forward pass over the sequence
+    _, out = jax.lax.scan(
+        lambda carry, input: f_repack(carry, input),
+        init=(hidden_state, jacobian_state),
+        xs=inputs,
+    )
+
+    return out
+
+
+def loss_func(
+    model: StackedRNN, inputs: Array, jacobian_state: eqx.nn.State, outputs: Array
+):
+    pred = forward_sequence(model, inputs, jacobian_state)
+    errors = jnp.sum((pred - outputs) ** 2, axis=1)
+    return jnp.sum(errors)
+
+
+def bptt(
+    model: StackedRNN, inputs: Array, jacobian_state: eqx.nn.State, outputs: Array
+):
+    loss, grads = eqx.filter_value_and_grad(loss_func)(
+        model, inputs, jacobian_state, outputs
+    )
+
+    return loss, grads
+
+
+T = 10
+num_layers = 2
+hidden_size = 10
+input_size = 10
 key = jax.random.PRNGKey(7)
 
-model, inmediate_jacobians_state = make_with_state(StackedRTRL)(
+model, inmediate_jacobians_state = make_with_state(StackedRNN)(
     key, num_layers=num_layers, hidden_size=hidden_size, input_size=input_size
 )
 
-model_rtrl, model_spatial = eqx.partition(
-    model,
-    lambda leaf: is_pta_cell_or_jacobian_index(leaf),
-    is_leaf=is_pta_cell_or_jacobian_index,
-)
+inputs = jnp.ones(shape=(T, input_size), dtype=jnp.float32)
+outputs = jnp.zeros(shape=(T, input_size), dtype=jnp.float32)
+loss, grads = bptt(model, inputs, inmediate_jacobians_state, outputs)
+loss_rtrl, grads_rtrl = rtrl(model, inputs, inmediate_jacobians_state, outputs)
 
-assert eqx.tree_equal(
-    eqx.combine(model_rtrl, model_spatial, is_leaf=is_pta_cell_or_jacobian_index), model
-)
+for (path_a, leaf_a), (path_b, leaf_b) in zip(
+    jax.tree_util.tree_leaves_with_path(grads),
+    jax.tree_util.tree_leaves_with_path(grads_rtrl),
+):
+    print(path_a, path_b)
+    print(jnp.allclose(leaf_a, leaf_b))
 
-jacobian = make_zeros_jacobian(model_rtrl)
-input = jnp.ones(shape=(input_size,))
-output = jnp.zeros(shape=(input_size,))
-h_prev = jnp.ones(shape=(num_layers, hidden_size))
-perturbations = jnp.zeros(shape=(num_layers, hidden_size))
+# As expected by the online setup the gradients of the first weight matrices
+# are not equal to bptt.
---
+++ rnn.py
@@ -0,0 +1,174 @@
+from typing import Any, List, Optional, Tuple, cast
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from jaxtyping import Array, Float32, PRNGKeyArray, PyTree, Scalar
+
+
+class RNN(eqx.Module):
+    weights_hh: Float32[Array, "n_hidden n_hidden"]
+    weights_ih: Float32[Array, "n_hidden input_dim"]
+    bias: Optional[Float32[Array, "n_hidden"]]
+    input_size: int = eqx.field(static=True)
+    hidden_size: int = eqx.field(static=True)
+    use_bias: bool = eqx.field(static=True)
+
+    def __init__(
+        self,
+        hidden_size: int,
+        input_size: int,
+        use_bias: bool = True,
+        *,
+        key: PRNGKeyArray,
+    ):
+        hhkey, ihkey, bkey = jax.random.split(key, 3)
+
+        # Use Glorot Initialization for the IH Matrix.
+        lim = jnp.sqrt(1 / hidden_size)
+
+        self.weights_hh = jax.random.uniform(
+            hhkey, shape=(hidden_size, input_size), minval=-lim, maxval=lim
+        )
+
+        self.weights_ih = jax.random.uniform(
+            ihkey, shape=(hidden_size, input_size), minval=-lim, maxval=lim
+        )
+
+        if use_bias:
+            self.bias = jax.random.uniform(
+                bkey, (hidden_size,), minval=-lim, maxval=lim
+            )
+        else:
+            self.bias = None
+
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.use_bias = use_bias
+
+    @jax.jit
+    def f(
+        self, h: Float32[Array, "hidden_size"], x: Float32[Array, "input_size"]
+    ) -> Float32[Array, "hidden_size"]:
+        if self.use_bias:
+            bias = self.bias
+        else:
+            bias = 0
+
+        h_new = self.weights_hh @ jnp.tanh(h) + (self.weights_ih @ x) + bias
+
+        return h_new
+
+    def __call__(
+        self, h: Float32[Array, "hidden_size"], x: Float32[Array, "input_size"]
+    ) -> Float32[Array, "hidden_size"]:
+        return self.f(h, x)
+
+
+def zero_influence_pytree(cell: RNN) -> RNN:
+    def zeros_jacobian(leaf):
+        return jnp.zeros(shape=(cell.hidden_size, *leaf.shape))
+
+    return jax.tree_map(zeros_jacobian, cell)
+
+
+class RNNLayerRTRL(eqx.Module):
+    cell: RNN
+    C: eqx.nn.Linear
+    D: eqx.nn.Linear
+    jacobian_index: eqx.nn.StateIndex
+
+    def __init__(
+        self, hidden_size: int, input_size: int, use_bias: bool, key: PRNGKeyArray
+    ):
+        cell_key, c_key, d_key = jax.random.split(key, 3)
+        self.cell = RNN(hidden_size, input_size, use_bias=use_bias, key=cell_key)
+        self.C = eqx.nn.Linear(hidden_size, hidden_size, use_bias=False, key=c_key)
+        self.D = eqx.nn.Linear(hidden_size, input_size, use_bias=False, key=d_key)
+        self.jacobian_index = eqx.nn.StateIndex(
+            (
+                zero_influence_pytree(self.cell),
+                jnp.zeros(shape=(self.cell.hidden_size, self.cell.hidden_size)),
+            )
+        )
+
+    def __call__(
+        self,
+        h_prev: Float32[Array, "ndim"],
+        input: Float32[Array, "ndim"],
+        perturbation: Float32[Array, "ndim"],
+        jacobian_state: eqx.nn.State,
+    ):
+        """
+        Returns h_(t), y_(t)
+        """
+        # To the RNN Cell
+        h_out = self.cell(h_prev, input) + perturbation
+
+        # Compute Jacobian and set them in the state.
+        jacobian_func = jax.jit(jax.jacrev(RNN.f, argnums=(0, 1)))
+        inmediate_jacobian, dynamics = jacobian_func(self.cell, h_prev, input)
+        new_jacobian_state = jacobian_state.set(
+            self.jacobian_index, (inmediate_jacobian, dynamics)
+        )
+
+        # Project out
+        y_out = self.C(jnp.tanh(h_out)) + self.D(input)
+
+        return h_out, y_out, new_jacobian_state
+
+
+class StackedRNN(eqx.Module):
+    """
+    It acts as a unique cell.
+    """
+
+    layers: List[RNNLayerRTRL]
+    num_layers: int = eqx.field(static=True)
+    hidden_size: int = eqx.field(static=True)
+    input_size: int = eqx.field(static=True)
+
+    def __init__(
+        self, key: PRNGKeyArray, num_layers: int, hidden_size: int, input_size: int
+    ):
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+
+        self.layers = []
+        keys = jax.random.split(key, num=num_layers + 1)
+        for i in range(num_layers):
+            layer = RNNLayerRTRL(hidden_size, hidden_size, use_bias=True, key=keys[i])
+            self.layers.append(layer)
+
+    def f(
+        self,
+        h_prev: Float32[Array, "num_layers hidden_size"],
+        input: Float32[Array, "ndim"],
+        perturbations: Float32[Array, "num_layers hidden_size"],
+        jacobians_state: eqx.nn.State,
+    ) -> Tuple[
+        Float32[Array, "num_layers hidden_size"],
+        Float32[Array, "hidden_size"],
+        eqx.nn.State,
+    ]:
+        h_collect: List[Array] = []
+        out = input
+        for i, cell in enumerate(self.layers):
+            h_out, out, jacobians_state = cell(
+                h_prev[i], out, perturbations[i], jacobians_state
+            )
+            h_collect.append(h_out)
+
+        h_new = jnp.stack(h_collect)
+
+        return h_new, out, jacobians_state
+
+    def __call__(
+        self,
+        h_prev: Float32[Array, "num_layers hidden_size"],
+        input: Float32[Array, "ndim"],
+        perturbations: Float32[Array, "num_layers hidden_size"],
+        jacobians_state: eqx.nn.State,
+    ):
+        return self.f(h_prev, input, perturbations, jacobians_state)
