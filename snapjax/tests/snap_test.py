a69bca6ed43eb37c4bd86a71c29f331d44dbecad
Kev Lopez <kevinlopezandrade@gmail.com>
2024-01-10 12:59:25 +0100

    Add dynamic snap-1 and sparse snap-1 test & Clean unncesary files.

---
+++ snapjax/tests/snap_test.py
@@ -0,0 +1,42 @@
+import equinox as eqx
+import jax.numpy as jnp
+import jax.tree_util as jtu
+from utils import get_random_input_sequence, get_stacked_rnn
+
+from snapjax.algos import rtrl
+
+ATOL = 1e-07
+RTOL = 0.0
+
+
+def test_jacobians():
+    T = 100
+    model = get_stacked_rnn(4, 20, 20, sparse=True, seed=7)
+    model_no_sp = get_stacked_rnn(4, 20, 20, sparse=False, seed=7)
+
+    inputs = get_random_input_sequence(T, model)
+    targets = get_random_input_sequence(T, model)
+
+    loss, acc_grads, _ = rtrl(model, inputs, targets, use_snap_1=True, use_scan=True)
+    print("--------------------------")
+    loss_no_sp, acc_grads_no_sp, _ = rtrl(
+        model_no_sp, inputs, targets, use_snap_1=True, use_scan=True
+    )
+
+    assert jnp.allclose(loss, loss_no_sp)
+
+
+    passed = True
+    for (key_a, leaf_a), (key_b, leaf_b) in zip(
+        jtu.tree_leaves_with_path(acc_grads),
+        jtu.tree_leaves_with_path(acc_grads_no_sp),
+    ):
+        if jnp.allclose(leaf_a, leaf_b, atol=ATOL, rtol=RTOL):
+            print("Match", jtu.keystr(key_a))
+        else:
+            print("Don't Match", jtu.keystr(key_a))
+            print("\tMax difference: ", jnp.max(jnp.abs(leaf_a - leaf_b)))
+            print("\tMean difference: ", jnp.mean(jnp.abs(leaf_a - leaf_b)))
+            passed = False
+
+    assert passed
