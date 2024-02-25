9323d00ec8c13f46e57aff654b31f4bd643b0d2c
Kev <kevinlopezandrade@gmail.com>
2024-02-25 22:39:18 +0100

    Add exp sum dataloader & Matrix Jacobian Sparse Test

---
+++ snapjax/dataloaders/exp_sum.py
@@ -0,0 +1,80 @@
+import jax
+import jax.numpy as jnp
+import jax.random as jrandom
+import numpy as np
+import scipy
+from jaxtyping import PRNGKeyArray
+from numpy.typing import NDArray
+
+
+def exp_sum_sequence_scipy(
+    key: PRNGKeyArray,
+    dt: float,
+    T: float,
+    c: NDArray[np.float32],
+    b: NDArray[np.float32],
+    W: NDArray[np.float32],
+):
+    times = np.arange(0, T + dt, step=dt)
+
+    # x(s)
+    variance = 1
+    mean = 0
+    x_discrete = np.sqrt(variance) * jrandom.normal(key, shape=times.shape) + mean
+
+    def p(t):
+        return c.T @ scipy.linalg.expm(W * t) @ b
+
+    p_discrete = np.array([p(time) for time in times])
+
+    y = np.convolve(p_discrete, x_discrete, mode="full") * dt
+    y = y[: len(times)]
+
+    mask = np.zeros(times.shape[0])
+    mask[-1] = 1.0
+
+    # test = np.trapz(p_discrete * x_discrete[::-1], dx=dt)
+
+    return x_discrete, y, mask
+
+
+def _convolution_with_white_noise(
+    key: PRNGKeyArray, dt: float, signal: NDArray, mean: int = 0, variance: float = 1.0
+):
+    x_discrete = jnp.sqrt(variance) * jrandom.normal(key, shape=signal.shape) + mean
+    y = jnp.convolve(signal, x_discrete, mode="full") * dt
+    y = y[: len(signal)]
+    return x_discrete, y
+
+
+def gen_exp_sum(key: PRNGKeyArray, N: int, m: int, dt: float, T: float):
+    with jax.default_device(jax.devices("cpu")[0]):
+        key, c_key, b_key, matrix_key = jrandom.split(key, 4)
+
+        # Kernel params
+        c = jrandom.normal(c_key, shape=(m,))
+        b = jrandom.normal(b_key, shape=(m,))
+
+        variance = 1 / m
+        Z = jrandom.normal(matrix_key, shape=(m, m))
+        Z = jnp.sqrt(variance) * Z
+        W = -1 * jnp.eye(Z.shape[0]) - (Z.T @ Z)
+
+        # Time steps
+        times = jnp.arange(0, T + dt, step=dt)
+
+        # Kernel
+        @jax.jit
+        def p(t):
+            return c.T @ jax.scipy.linalg.expm(W * t) @ b
+
+        p_discrete = jax.vmap(p)(times)
+
+        # Mask
+        mask = jnp.ones(len(times))
+        mask = mask.at[-1].set(1.0)
+
+        keys = jrandom.split(key, N)
+        for key in keys:
+            inp, out = _convolution_with_white_noise(key, dt=dt, signal=p_discrete)
+            yield inp, out, mask
---
+++ snapjax/tests/matrix_jacobian_test.py
@@ -0,0 +1,33 @@
+import time
+
+import jax
+import jax.numpy as jnp
+import jax.random as jrandom
+from jax.experimental.sparse import BCOO
+
+from snapjax.sp_jacrev import make_jacobian_projection, sp_jacrev
+
+
+def test_matrix_jacobian_sparse():
+
+    def f(W, h):
+        res = W @ h
+        res = res.at[0].set(res[0] + W[1, 1] * 5.0)
+        return res
+
+    seed = int(time.time() * 1000)
+    w_key, h_key = jrandom.split(jrandom.PRNGKey(seed), 2)
+    W = jax.random.normal(w_key, (4, 4))
+    h = jax.random.normal(h_key, (4,))
+
+    jacobian = jax.jacobian(f)(W, h)
+
+    sp = BCOO.fromdense((jnp.abs(jacobian) > 0.0).astype(jnp.float32).reshape(4, -1))
+    projection = make_jacobian_projection(sp)
+
+    partial = jax.tree_util.Partial(f, h=h)
+    sp_jacobian_func = sp_jacrev(partial, projection)
+    sp_jacobian = sp_jacobian_func(W)
+    # print(sp_jacobian.todense().reshape(4, 4, 4))
+
+    assert jnp.allclose(jacobian.reshape(jacobian.shape[0], -1), sp_jacobian.todense())
