90fc19b994fd54408bc4385608f887573dbab259
Kev <kevinlopezandrade@gmail.com>
2024-03-18 16:11:51 +0100

    Allow data generator to return its params.

--- snapjax/dataloaders/ssm.py
+++ snapjax/dataloaders/ssm.py
@@ -35,35 +35,54 @@ def check_log_existence(M: ArrayLike):
     return np.allclose(np.imag(val), 0)
 
 
-def _gen_ssm(key: PRNGKeyArray, N: int, h: int, T: int):
+def _gen_ssm(
+    key: PRNGKeyArray,
+    h: int,
+    *,
+    with_params: bool = False,
+    minval: int = -1,
+    maxval: int = 0
+):
     with jax.default_device(jax.devices("cpu")[0]):
-        key, c_key, b_key, matrix_key = jrandom.split(key, 4)
+        data_key, c_key, b_key, matrix_key = jrandom.split(key, 4)
         c = jrandom.normal(c_key, shape=(h,))
         b = jrandom.normal(b_key, shape=(h,))
 
         # Generate recurrence matrix.
         d_key, p_key = jrandom.split(matrix_key, 2)
-        D = jnp.diag(jrandom.uniform(d_key, shape=(h,), minval=-1, maxval=0))
+        D = jnp.diag(jrandom.uniform(d_key, shape=(h,), minval=minval, maxval=maxval))
         P = jrandom.normal(p_key, shape=(h, h))
         P_inv = jnp.linalg.inv(P)
         W = P @ expm(D) @ P_inv
 
         assert check_log_existence(W)
 
-        kernel = K_conv(W, b, c, T)
+        # Params
+        params = {
+            "A": W,
+            "B": b,
+            "C": c,
+        }
 
-        keys = jrandom.split(key, N)
+        def generator(N: int, T: int):
+            keys = jrandom.split(data_key, N)
+            kernel = K_conv(W, b, c, T)
+            mask = jnp.ones(T)
+            for key in keys:
+                inp = jrandom.normal(key, shape=(T,))
+                out = causal_convolution(inp, kernel)
+                inp = inp.reshape(inp.shape[0], 1)
+                out = out.reshape(out.shape[0], 1)
 
-        mask = jnp.ones(T)
-        for key in keys:
-            inp = jrandom.normal(key, shape=(T,))
-            out = causal_convolution(inp, kernel)
-            inp = inp.reshape(inp.shape[0], 1)
-            out = out.reshape(out.shape[0], 1)
+                yield inp, out, mask
 
-            yield inp, out, mask
+        if with_params:
+            return params, generator
+        else:
+            return generator
 
 
-def gen_ssm(key: PRNGKeyArray, N: int, **params):
-    for inp, out, mask in _gen_ssm(key, N=N, **params):
-        yield inp, out, mask
+def gen_ssm(key: PRNGKeyArray, N: int, T: int, **params):
+    generator = _gen_ssm(key, with_params=False, **params)
+    for sample in generator(N=N, T=T):
+        yield sample
